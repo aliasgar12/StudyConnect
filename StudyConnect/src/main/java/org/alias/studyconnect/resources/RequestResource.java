4a9e1d924471791b43ed78cf9f5155772171010f
Ali Asgar <ali.asgar26@gmail.com>
2017-04-27 05:52:21 -0400

    adding service and resources to fetch accepted request

--- StudyConnect/src/main/java/org/alias/studyconnect/resources/RequestResource.java
+++ StudyConnect/src/main/java/org/alias/studyconnect/resources/RequestResource.java
@@ -21,78 +21,79 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class RequestResource {
-	
+
 	private RequestService requestService;
-	
+
 	// Get my received request
 	@Path("received")
 	@GET
-	public Response myReceivedRequest(@PathParam ("userId") int userId) throws JsonProcessingException{
+	public Response myReceivedRequest(@PathParam("userId") int userId) throws JsonProcessingException {
 		requestService = new RequestService();
 		String result = requestService.getReceivedRequest(userId);
-		return Response.status(Status.OK)
-						.entity(result)
-						.build();
+		return Response.status(Status.OK).entity(result).build();
 	}
-	
-	//Get Sent Request
+
+	// Get my received request
+	@Path("accepted")
+	@GET
+	public Response myAcceptedRequest(@PathParam("userId") int userId) throws JsonProcessingException {
+		requestService = new RequestService();
+		String result = requestService.getAcceptedRequest(userId);
+		if(result == "")
+			return Response.status(Status.NOT_FOUND).build();
+		return Response.status(Status.OK).entity(result).build();
+	}
+
+	// Get Sent Request
 	@Path("sent")
 	@GET
-	public Response mySentRequest(@PathParam ("userId") int userId) throws JsonProcessingException{
+	public Response mySentRequest(@PathParam("userId") int userId) throws JsonProcessingException {
 		requestService = new RequestService();
 		String result = requestService.getSentRequest(userId);
-		return Response.status(Status.OK)
-						.entity(result)
-						.build();
+		return Response.status(Status.OK).entity(result).build();
 	}
-	
-	/*	Create a new request in the request table with the request object recieved
-		Add the request to the corresponding sent user hashtable
-		Add the request to the corresponding received user hashtable
-		Add to subjects list
-		Throw incomplete if any of the steps fails
-	*/
+
+	/*
+	 * Create a new request in the request table with the request object
+	 * recieved Add the request to the corresponding sent user hashtable Add the
+	 * request to the corresponding received user hashtable Add to subjects list
+	 * Throw incomplete if any of the steps fails
+	 */
 	@POST
-	public Response addRequest (Request request){
+	public Response addRequest(Request request) {
 		requestService = new RequestService();
 		int result = requestService.addRequest(request);
-		if(result == 0)
+		if (result == 0)
 			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-		else if(result == 409)
+		else if (result == 409)
 			return Response.status(Status.CONFLICT).build();
-		return Response.status(Status.OK)
-						.build();
+		return Response.status(Status.OK).build();
 	}
-	
-	//	Completing or rejecting a request
-	//	Delete the request with this primary key from the module table
-	//	Delete it from both the requesting and the sent user hash set
-	//	Close the chat room
-	//	Remove users from each others request list
+
+	// Completing or rejecting a request
+	// Delete the request with this primary key from the module table
+	// Delete it from both the requesting and the sent user hash set
+	// Close the chat room
+	// Remove users from each others request list
 	@DELETE
-	public Response deleteRequest (Request request){
+	public Response deleteRequest(Request request) {
 		requestService = new RequestService();
 		int result = requestService.deleteRequest(request);
-		if(result == 1)
-			return Response.status(Status.OK)
-						.build();
-			return Response.status(Status.INTERNAL_SERVER_ERROR)
-						.build();
-			}
-	
-	
-//	Set the corresponding request flag to 1."
-//	Change accept to complete in mobile"
-//	Add the user to the chat list of both the users. "
-	//Accepting a request
+		if (result == 1)
+			return Response.status(Status.OK).build();
+		return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+	}
+
+	// Set the corresponding request flag to 1."
+	// Change accept to complete in mobile"
+	// Add the user to the chat list of both the users. "
+	// Accepting a request
 	@Path("accept")
 	@PUT
-	public Response acceptRequest(Request request){
+	public Response acceptRequest(Request request) {
 		requestService = new RequestService();
 		int result = requestService.acceptRequest(request);
-		return Response.status(Status.OK)
-					   .build();
+		return Response.status(Status.OK).build();
 	}
-	
 
 }
--- StudyConnect/src/main/java/org/alias/studyconnect/services/RequestService.java
+++ StudyConnect/src/main/java/org/alias/studyconnect/services/RequestService.java
@@ -34,8 +34,8 @@ public class RequestService {
 		em.getTransaction().begin();
 		UserDetails user = em.find(UserDetails.class, userId);
 		Set<Request> requests = new HashSet<>();
-		for(Request req : user.getReqReceived())
-			if(req.getFlag()==0)
+		for (Request req : user.getReqReceived())
+			if (req.getFlag() == 0)
 				requests.add(req);
 		objectMapper = new ObjectMapper();
 		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
@@ -115,16 +115,20 @@ public class RequestService {
 		return 1;
 	}
 
-//	private void doDelOperations(Request request)
-//			throws NotFoundException, TypeMismatchException, IllegalArgumentException {
-//		UserDetails toUser = em.find(UserDetails.class, request.getRequestId().getToUserId());
-//		UserDetails fromUser = em.find(UserDetails.class, request.getRequestId().getFromUserId());
-//		Module module = em.find(Module.class, request.getRequestId().getModuleId());
-//		toUser.getReqReceived().remove(request);
-//		fromUser.getReqSent().remove(request);
-//		module.getRequestList().remove(request);
-//
-//	}
+	// private void doDelOperations(Request request)
+	// throws NotFoundException, TypeMismatchException, IllegalArgumentException
+	// {
+	// UserDetails toUser = em.find(UserDetails.class,
+	// request.getRequestId().getToUserId());
+	// UserDetails fromUser = em.find(UserDetails.class,
+	// request.getRequestId().getFromUserId());
+	// Module module = em.find(Module.class,
+	// request.getRequestId().getModuleId());
+	// toUser.getReqReceived().remove(request);
+	// fromUser.getReqSent().remove(request);
+	// module.getRequestList().remove(request);
+	//
+	// }
 
 	public int acceptRequest(Request request) {
 		try {
@@ -155,13 +159,38 @@ public class RequestService {
 
 	private void sendNotification(Request request)
 			throws NotFoundException, TypeMismatchException, IllegalArgumentException {
-		// TODO Auto-generated method stub
 		UserDetails toUser = em.find(UserDetails.class, request.getRequestId().getToUserId());
 		Module module = em.find(Module.class, request.getRequestId().getModuleId());
 		em.merge(request);
 		NotificationServer.getInstance(request.getToUserName(), module.getModuleName(),
 				module.getSubjectId().getSubjectName(), toUser.getApp_token(), ACCEPT_REQUEST).sendRequest();
 
+	}
+
+	public String getAcceptedRequest(int userId) {
+		String result;
+		try {
+			em = EntityUtil.getEntityManager();
+
+			em.getTransaction().begin();
+			UserDetails user = em.find(UserDetails.class, userId);
+			Set<Request> requests = new HashSet<>();
+			for (Request req : user.getReqReceived())
+				if (req.getFlag() == 1)
+					requests.add(req);
+			objectMapper = new ObjectMapper();
+			objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+			result = objectMapper.writeValueAsString(requests);
+			em.getTransaction().commit();
+			em.close();
+		} catch (RollbackException e) {
+			e.printStackTrace();
+			return "";
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return "";
+		}
+		return result;
 	}
 
 }
